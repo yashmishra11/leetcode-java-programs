ecc662304cbfe2dd6df27a985643c6e1ba82cc41
Yash Mishra <136472014+yashmishra11@users.noreply.github.com>
2026-04-03 18:00:46 +0530

    Update AvgofNnums.java

--- AvgofNnums.java
+++ AvgofNnums.java
@@ -3,21 +3,23 @@ import java.util.Scanner;
 public class AvgofNnums {
     public static void main(String[] args) {
         int count;
-        System.out.println("enter the count of numbers: ");
+
+        System.out.print("enter the count : ");
         Scanner sc = new Scanner(System.in);
         count = sc.nextInt();
-        System.out.println("enter the numbers: ");
+
+        System.out.print("enter the numbers : ");
 
         int number, sum = 0;
-        float average = 0;
+        float average;
+
         for (int i = 0; i < count; i++) {
             number = sc.nextInt();
             sum = sum + number;
         }
         average = (float) sum / count;
-        System.out.println("the average is: " + average);
+        System.out.println("the sum is : " + sum);
+        System.out.println("the average is : " + average);
         sc.close();
     }
-
 }
-//adds numbers as i input them and at the end of loop calculates average
