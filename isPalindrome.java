272506688b649abd9797dfa7c5abe5b8466adc41
Yash Mishra <136472014+yashmishra11@users.noreply.github.com>
2026-04-30 16:47:59 +0530

    Create isPalindrome.java

---
+++ isPalindrome.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class Solution {
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {//skipping if not char
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {//skipping if not char
+                right--;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        if (isPalindrome(input)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+        sc.close();
+    }
+}
