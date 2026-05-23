fd8777ab7147488d1fee6b8ea6a7617b89e8611c
Yash Mishra <136472014+yashmishra11@users.noreply.github.com>
2026-05-23 06:51:13 +0530

    Create climbStairs.java

---
+++ climbStairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+
+        if (n==1){
+            return 1;
+        }
+        int first = 1;
+        int second = 2;
+
+        for(int i=3;i<=n;i++){
+            int  current = first+second;
+
+            first= second;
+            second= current;
+
+            
+        }
+        return second;
+    }  
+}
