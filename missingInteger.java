a07665045113686004f57a3bfa5970ec58670b21
Yash Mishra <136472014+yashmishra11@users.noreply.github.com>
2026-08-11 07:32:25 +0530

    Create missingInteger.java

---
+++ missingInteger.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int missingInteger(int[] nums) {
+      int sum = nums[0];
+
+      for (int i = 1; i < nums.length; i++){
+        if (nums[i] == nums[i-1]+1)
+            sum += nums[i];
+
+            else
+                break;
+      }  
+
+      while (true){
+        boolean found = false;
+
+        for (int num:  nums){
+            if (num == sum){
+            found = true;
+            break;
+        }
+      }
+
+      if (!found)
+        return sum;
+
+        sum++;
+      }
+    }
+}
