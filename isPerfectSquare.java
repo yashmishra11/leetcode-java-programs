cf4701c759556356fbdc14fc706febdcfb238f3a
Yash Mishra <136472014+yashmishra11@users.noreply.github.com>
2026-09-17 07:10:46 +0530

    Create isPerfectSquare.java

---
+++ isPerfectSquare.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        
+        long left = 1;
+        long right = num;
+
+        while(left <= right){
+
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
+
+            if (square == num ){
+                return true;
+            }
+            if (square < num){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+}
